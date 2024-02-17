3eb9f7efe59d4a20b6fe20efab86a27b59248927
LetaWang <letahwang@gmail.com>
2024-02-17 10:56:28 -0600

    bluetooth changes

--- useBLE.ts
+++ useBLE.ts
@@ -1,182 +1,189 @@
-/* eslint-disable no-bitwise */
+/**
+ * @file useBLE.ts
+ * @brief Custom hook for bluetooth low energy
+ * most code is from https://github.com/friyiajr/BLESampleExpo
+ * changed to handle data taken from Seeed Studio XIAO nrf52840 microcontroller 11/29/2023
+ */
+
 import { useMemo, useState } from "react";
 import { PermissionsAndroid, Platform } from "react-native";
+import "@react-native-anywhere/polyfill-base64";
 import {
-  BleError,
-  BleManager,
-  Characteristic,
-  Device,
+    BleError,
+    BleManager,
+    Characteristic,
+    Device,
 } from "react-native-ble-plx";
 
 import * as ExpoDevice from "expo-device";
 
-import base64 from "react-native-base64";
+const ENVIRONMENTAL_SENSING_UUID = "181A"; // environmental sensing service uuid
+const UUID16_CHR_UV_INDEX = "2A76"; // uv index characteristic uuid
 
-// Also need to change this 
-const HEART_RATE_UUID = "181A";
-const HEART_RATE_CHARACTERISTIC = "2A76";
-
-interface BluetoothLowEnergyApi {
-  requestPermissions(): Promise<boolean>;
-  scanForPeripherals(): void;
-  connectToDevice: (deviceId: Device) => Promise<void>;
-  disconnectFromDevice: () => void;
-  connectedDevice: Device | null;
-  allDevices: Device[];
-  heartRate: number;
+interface BluetoothLowEnergyApi { // interface for bluetooth low energy api
+    requestPermissions(): Promise<boolean>;
+    scanForPeripherals(): void;
+    connectToDevice: (deviceId: Device) => Promise<void>;
+    disconnectFromDevice: () => void;
+    connectedDevice: Device | null;
+    allDevices: Device[];
+    uvIndex: string;
 }
 
 function useBLE(): BluetoothLowEnergyApi {
-  const bleManager = useMemo(() => new BleManager(), []);
-  const [allDevices, setAllDevices] = useState<Device[]>([]);
-  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
-  const [heartRate, setHeartRate] = useState<number>(0);
+    const bleManager = useMemo(() => new BleManager(), []); // create a new instance of BleManager
+    const [allDevices, setAllDevices] = useState<Device[]>([]); // array of all devices found
+    const [connectedDevice, setConnectedDevice] = useState<Device | null>(null); // device that is connected
+    const [uvIndex, setUVIndex] = useState<string>(""); // UV Index
 
-  const requestAndroid31Permissions = async () => {
-    const bluetoothScanPermission = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
-      {
-        title: "Location Permission",
-        message: "Bluetooth Low Energy requires Location",
-        buttonPositive: "OK",
-      }
-    );
-    const bluetoothConnectPermission = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
-      {
-        title: "Location Permission",
-        message: "Bluetooth Low Energy requires Location",
-        buttonPositive: "OK",
-      }
-    );
-    const fineLocationPermission = await PermissionsAndroid.request(
-      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-      {
-        title: "Location Permission",
-        message: "Bluetooth Low Energy requires Location",
-        buttonPositive: "OK",
-      }
-    );
-
-    return (
-      bluetoothScanPermission === "granted" &&
-      bluetoothConnectPermission === "granted" &&
-      fineLocationPermission === "granted"
-    );
-  };
-
-  const requestPermissions = async () => {
-    if (Platform.OS === "android") {
-      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          {
-            title: "Location Permission",
-            message: "Bluetooth Low Energy requires Location",
-            buttonPositive: "OK",
-          }
+    const requestAndroid31Permissions = async () => { // request permissions for android 31
+        const bluetoothScanPermission = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+            {
+                title: "Location Permission",
+                message: "Bluetooth Low Energy requires Location",
+                buttonPositive: "OK",
+            }
         );
-        return granted === PermissionsAndroid.RESULTS.GRANTED;
-      } else {
-        const isAndroid31PermissionsGranted =
-          await requestAndroid31Permissions();
+        const bluetoothConnectPermission = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+            {
+                title: "Location Permission",
+                message: "Bluetooth Low Energy requires Location",
+                buttonPositive: "OK",
+            }
+        );
+        const fineLocationPermission = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            {
+                title: "Location Permission",
+                message: "Bluetooth Low Energy requires Location",
+                buttonPositive: "OK",
+            }
+        );
 
-        return isAndroid31PermissionsGranted;
-      }
-    } else {
-      return true;
-    }
-  };
+        return (
+            bluetoothScanPermission === "granted" &&
+            bluetoothConnectPermission === "granted" &&
+            fineLocationPermission === "granted"
+        );
+    };
 
-  const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
-    devices.findIndex((device) => nextDevice.id === device.id) > -1;
+    const requestPermissions = async () => { // request permissions for android 31 and below
+        if (Platform.OS === "android") {
+            if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+                const granted = await PermissionsAndroid.request(
+                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                    {
+                        title: "Location Permission",
+                        message: "Bluetooth Low Energy requires Location",
+                        buttonPositive: "OK",
+                    }
+                );
+                return granted === PermissionsAndroid.RESULTS.GRANTED;
+            } else {
+                const isAndroid31PermissionsGranted =
+                    await requestAndroid31Permissions();
 
-  const scanForPeripherals = () =>
-    bleManager.startDeviceScan(null, null, (error, device) => {
-      if (error) {
-        console.log(error);
-      }
-      if (device) {
-        setAllDevices((prevState: Device[]) => {
-          if (!isDuplicteDevice(prevState, device)) {
-            return [...prevState, device];
-          }
-          return prevState;
+                return isAndroid31PermissionsGranted;
+            }
+        } else {
+            return true;
+        }
+    };
+
+    const isDuplicteDevice = (devices: Device[], nextDevice: Device) =>
+        devices.findIndex((device) => nextDevice.id === device.id) > -1;
+
+    const scanForPeripherals = () =>
+        bleManager.startDeviceScan(null, null, (error, device) => {
+            if (error) {
+                console.log(error);
+            }
+            if (device && device.name?.includes("XIAO")) {
+                setAllDevices((prevState: Device[]) => {
+                    if (!isDuplicteDevice(prevState, device)) {
+                        return [...prevState, device];
+                    }
+                    return prevState;
+                });
+            }
         });
-      }
-    });
 
-  const connectToDevice = async (device: Device) => {
-    try {
-      const deviceConnection = await bleManager.connectToDevice(device.id);
-      setConnectedDevice(deviceConnection);
-      await deviceConnection.discoverAllServicesAndCharacteristics();
-      bleManager.stopDeviceScan();
-      startStreamingData(deviceConnection);
-    } catch (e) {
-      console.log("FAILED TO CONNECT", e);
-    }
-  };
+    const connectToDevice = async (device: Device) => {
+        try {
+            const deviceConnection = await bleManager.connectToDevice(
+                device.id, { autoConnect: true }
+            );
+            setConnectedDevice(deviceConnection);
+            await deviceConnection.discoverAllServicesAndCharacteristics();
+            bleManager.stopDeviceScan();
+            startStreamingData(deviceConnection);
+        } catch (e) {
+            console.log("FAILED TO CONNECT", e);
+        }
+    };
 
-  const disconnectFromDevice = () => {
-    if (connectedDevice) {
-      bleManager.cancelDeviceConnection(connectedDevice.id);
-      setConnectedDevice(null);
-      setHeartRate(0);
-    }
-  };
+    const disconnectFromDevice = () => {
+        if (connectedDevice) {
+            bleManager.cancelDeviceConnection(connectedDevice.id);
+            setConnectedDevice(null);
+            setUVIndex("");
+        }
+    };
 
-  const onHeartRateUpdate = (
-    error: BleError | null,
-    characteristic: Characteristic | null
-  ) => {
-    if (error) {
-      console.log(error);
-      return -1;
-    } else if (!characteristic?.value) {
-      console.log("No Data was recieved");
-      return -1;
-    }
+    const onUVIndexUpdate = (
+        error: BleError | null,
+        characteristic: Characteristic | null
+    ) => {
+        if (error) {
+            console.log(error);
+            return -1;
+        } else if (!characteristic?.value) {
+            console.log("No Data was recieved");
+            return -1;
+        }
 
-    // This is where i need to update
+        const raw = atob(characteristic.value); // convert base64 to raw binary data held in a string
+        let hexString = "";
+        for (let i = 0; i < raw.length; i++) { // convert base64 to hex string
+            const hex = raw.charCodeAt(i).toString(16);
+            hexString += hex.length === 2 ? hex : "0" + hex;
+        }
+        hexString = hexString.toUpperCase();
 
-    const rawData = base64.decode(characteristic.value);
-    let innerHeartRate: number = +rawData;
+        const fromHexString = (hexString: string) => // convert hex string to Uint8Array
+            Uint8Array.from(
+                hexString!.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16))
+            );
 
-    // const firstBitValue: number = Number(rawData) & 0x01;
+        const data = fromHexString(hexString)[0].toString(); // convert Uint8Array to string
 
-    // if (firstBitValue === 0) {
-    //   innerHeartRate = rawData[1].charCodeAt(0);
-    // } else {
-    //   innerHeartRate =
-    //     Number(rawData[1].charCodeAt(0) << 8) +
-    //     Number(rawData[2].charCodeAt(2));
-    // }
+        setUVIndex(data); // set UV Index
+    };
 
-    setHeartRate(innerHeartRate);
-  };
+    const startStreamingData = async (device: Device) => {
+        if (device) {
+            device.monitorCharacteristicForService(
+                ENVIRONMENTAL_SENSING_UUID,
+                UUID16_CHR_UV_INDEX,
+                onUVIndexUpdate
+            );
+            // device.writeCharacteristicWithResponseForService('12ab', '34cd', 'aGVsbG8gbWlzcyB0YXBweQ==') // write to characteristic
+        } else {
+            console.log("No Device Connected");
+        }
+    };
 
-  const startStreamingData = async (device: Device) => {
-    if (device) {
-      device.monitorCharacteristicForService(
-        HEART_RATE_UUID,
-        HEART_RATE_CHARACTERISTIC,
-        onHeartRateUpdate
-      );
-    } else {
-      console.log("No Device Connected");
-    }
-  };
-
-  return {
-    scanForPeripherals,
-    requestPermissions,
-    connectToDevice,
-    allDevices,
-    connectedDevice,
-    disconnectFromDevice,
-    heartRate,
-  };
+    return {
+        scanForPeripherals,
+        requestPermissions,
+        connectToDevice,
+        allDevices,
+        connectedDevice,
+        disconnectFromDevice,
+        uvIndex: uvIndex,
+    };
 }
 
 export default useBLE;
