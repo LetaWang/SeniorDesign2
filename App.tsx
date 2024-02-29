c9bef0c0501a71d8357fade64854587989184476
LetaWang <letahwang@gmail.com>
2024-02-28 22:51:58 -0600

    Trying to get old working new

--- App.tsx
+++ App.tsx
@@ -4,26 +4,22 @@ import MainStyles from './stylesheets/MainStyles.js'; // Import the stylesheet
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import BluetoothComponent from './Screens/bluetooth/BluetoothComponent.js';
-import HomeScreen from './Screens/HomeScreen.js';
-import UVIndex from './Screens/UVIndexScreen.js';
-import History from './Screens/HistoryScreen.js';
+// import HomeScreen from './Screens/HomeScreen.js';
+// import UVIndex from './Screens/UVIndexScreen.js';
+// import History from './Screens/HistoryScreen.js';
 import Profile from './Screens/ProfileScreen.js';
 
 const Stack = createStackNavigator();
 
-function App() {
+const App = () => {
 
   return (
    <NavigationContainer>
       <Stack.Navigator initialRouteName="Profile">
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }}/>
-        <Stack.Screen name="History" component={History} options={{ headerShown: false }}/>
-        <Stack.Screen name="Bluetooth" component={BluetoothComponent} options={{ headerShown: false }}/>
-        <Stack.Screen name="UVIndex" component={UVIndex} options={{ headerShown: false }}/>
         <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
